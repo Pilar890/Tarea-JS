1cf53e0c767c48600d37ec6e26f2283f2b8cbc3d
Pilar890 <pilarvillarreal546@gmail.com>
2023-02-01 01:54:24 -0300

    Primer commit task2

---
+++ upcoming-events.js
@@ -0,0 +1,25 @@
+const cards = document.getElementById('cont-card')
+let infoEvent = data.events
+let currentDate = data.currentDate
+let boxCards = ''
+
+for(let infooEvent of infoEvent){
+    if(currentDate < infooEvent.date ){
+        boxCards += `<section class="sec2">
+                            <div class="cards">
+                                <div class="img-card-cont">
+                                    <img class="festival" src=${infooEvent.image} alt="feria">
+                                </div>
+                                <h5>${infooEvent.name}</h5>
+                                <p class="txt">${infooEvent.description}</p>
+                                <div class="price">
+                                    <p>Price $${infooEvent.price}</p>
+                                    <a href="./details.html">Details</a>
+                                </div>
+                            </div>
+                        </section>` 
+
+    }
+ 
+}
+cards.innerHTML = boxCards
